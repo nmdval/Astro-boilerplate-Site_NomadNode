ae1173aee04ca2163738dad4d1c83d68349f0ae7
NOMAD <50786629+nomad7617@users.noreply.github.com>
2023-05-05 14:53:26 +0700

    Update ProjectList.tsx

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -14,30 +14,6 @@ const ProjectList = () => (
       </>
     }
   >
-    <div className="flex flex-col gap-6">
-      <div class="flex flex-col items-center gap-x-8 rounded-md bg-slate-800 p-3 md:flex-row">
-                <div class="shrink-0">
-                    <a href="https://aptoslabs.com">
-                        <img class="m-2 p-2 h-36 w-36 bg-white rounded-full" src="/build/images/aptos_logo.svg" loading="lazy">
-                    </a>
-                </div>
-                <div>
-                    <div class="flex flex-col items-center gap-y-2 md:flex-row">
-                        <a class="hover:text-cyan-400" href="https://aptoslabs.com">
-                            <div class="text-3xl font-semibold">Aptos</div>
-                        </a>
-                    </div>
-                    <p class="mt-3 text-xl text-gray-400">
-                        Aptos is the safe and scalable layer 1 blockchain. Built with Move, the language developed for the Aptos network.
-                    </p>
-
-                    <div class="mt-3 flex flex-col items-center gap-y-2 md:flex-row">
-                        <a class="px-6 py-2 rounded bg-slate-900 hover:bg-slate-700 text-slate-100" href="https://explorer.aptoslabs.com/validators?network=mainnet">
-                            <span class="text-md font-semibold">Validators</span>
-                        </a>
-                    </div>
-                </div>
-             </div>
       <Project
         name="Project 1"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
@@ -69,9 +45,9 @@ const ProjectList = () => (
         img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
         category={
           <>
-            <Tags color={ColorTags.VIOLET}>Next.js</Tags>
-            <Tags color={ColorTags.EMERALD}>Blog</Tags>
-            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
+            <a class="px-6 py-2 rounded bg-slate-900 hover:bg-slate-700 text-slate-100" href="https://explorer.aptoslabs.com/validators?network=mainnet">
+              <span class="text-md font-semibold">Validators</span>
+            </a>
           </>
         }
       />
